f2baf2def27749ce72710f32da01e4a3564f33aa
Steveng1997 <stivengarcia1855@gmail.com>
2023-06-03 11:39:59 -0500

    Ajuste sidevar

--- src/app/components/dashboard/navigation/sidenav-wrapper/sidenav-wrapper.component.ts
+++ src/app/components/dashboard/navigation/sidenav-wrapper/sidenav-wrapper.component.ts
@@ -1,24 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { NavigationEnd, Router, ActivatedRoute } from '@angular/router';
 import { LoginService } from 'src/app/core/services/login';
-import Swal from 'sweetalert2';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { MatSidenav } from '@angular/material/sidenav';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { delay, filter } from 'rxjs/operators';
 
+@UntilDestroy()
 @Component({
   selector: 'app-sidenav-wrapper',
   templateUrl: './sidenav-wrapper.component.html',
-  styleUrls: ['./sidenav-wrapper.component.css']
+  styleUrls: ['./sidenav-wrapper.component.css'],
+  // encapsulation: ViewEncapsulation.None  
 })
-export class SidenavWrapperComponent implements OnInit {
 
-  isExpanded: boolean = false;
-  isLiquidacion: boolean = false;
+export class SidenavWrapperComponent implements OnInit{
+
+  // isExpanded: boolean = false;
+  // isLiquidacion: boolean = false;
   usuarios: any[] = [];
-  idUser: string;
+  idUser: any;
+
+  @ViewChild(MatSidenav)
+  sidenav!: MatSidenav;
 
   constructor(
     public router: Router,
     private activeRoute: ActivatedRoute,
-    private serviceLogin: LoginService
+    private serviceLogin: LoginService,
+    private observer: BreakpointObserver
   ) { }
 
   ngOnInit(): void {
@@ -26,5 +36,21 @@ export class SidenavWrapperComponent implements OnInit {
     this.serviceLogin.getById(this.idUser).then((rp) => {
       this.idUser = rp[0]
     })
+  }
+
+  ngAfterViewInit() {
+
+    this.observer
+      .observe(['(max-width: 800px)'])
+      .pipe(delay(1), untilDestroyed(this))
+      .subscribe((res) => {
+        if (res.matches) {
+          this.sidenav.mode = 'over';
+          this.sidenav.close();
+        } else {
+          this.sidenav.mode = 'side';
+          this.sidenav.open();
+        }
+      });
   }
 }
