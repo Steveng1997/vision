54795746544c3c1ce9aa1feb5f58325457925ad0
StevenG1997 <stivengarcia1855@gmail.com>
2023-10-27 14:27:53 -0500

    change sidenav 10

--- src/app/components/dashboard/navigation/sidenav-wrapper/sidenav-wrapper.component.ts
+++ src/app/components/dashboard/navigation/sidenav-wrapper/sidenav-wrapper.component.ts
@@ -11,8 +11,6 @@ import { ServiceManager } from 'src/app/core/services/manager'
 export class SidenavWrapperComponent implements OnInit {
   public idUser: any
 
-  isLiquidation = false
-
   constructor(
     public router: Router,
     private activeRoute: ActivatedRoute,
@@ -20,46 +18,11 @@ export class SidenavWrapperComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    document.getElementById('my-class2').style.display = 'none'
-    document.getElementById('my-class3').style.display = 'none'
-    document.getElementById('my_back').style.display = 'none'
-
     this.idUser = this.activeRoute.snapshot.params;
     if (this.idUser.id) {
       this.serviceManager.getById(this.idUser.id).subscribe((res) => {
         this.idUser = res[0]
       })
-    }
-  }
-
-  liquidation() {
-    this.isLiquidation = !this.isLiquidation
-  }
-
-  click() {
-    var myClasses = document.querySelectorAll('.my-class'),
-      i = 0,
-      l = myClasses.length;
-
-    document.getElementById('my-class2').style.display = ''
-    document.getElementById('my-class3').style.display = ''
-    document.getElementById('my_back').style.display = ''
-
-    for (i; i < l; i++) {
-      myClasses[i]["style"].display = 'none';
-    }
-  }
-
-  back() {
-    var myClasses = document.querySelectorAll('.my-class'),
-      i = 0,
-      l = myClasses.length;
-
-    for (i; i < l; i++) {
-      myClasses[i]["style"].display = '';
-      document.getElementById('my-class2').style.display = 'none'
-      document.getElementById('my-class3').style.display = 'none'
-      document.getElementById('my_back').style.display = 'none'
     }
   }
 }
