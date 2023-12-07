c724d0605d8e08216211558e47a8e3aedc20f96c
StevenG1997 <stivengarcia1855@gmail.com>
2023-12-07 07:53:29 -0500

    adjust all 0.140

--- src/app/core/pipe/fecha-inicial.pipe.ts
+++ src/app/core/pipe/fecha-inicial.pipe.ts
@@ -12,9 +12,9 @@ export class FechaInicialPipe implements PipeTransform {
 
     if (items) {
       if (paramFechaInicial === undefined && paramFechaFinal === undefined) return
-      if (paramFechaInicial === undefined) return items.filter((item, index) => item.fecha <= paramFechaFinal)
-      if (paramFechaFinal === undefined) return items.filter((item, index) => item.fecha === paramFechaInicial)
-      return items.filter((item, index) => item.fecha >= paramFechaInicial && item.fecha <= paramFechaFinal)
+      if (paramFechaInicial === undefined) return items.filter((item, index) => item.fechaHoyInicio <= paramFechaFinal)
+      if (paramFechaFinal === undefined) return items.filter((item, index) => item.fechaHoyInicio === paramFechaInicial)
+      return items.filter((item, index) => item.fechaHoyInicio >= paramFechaInicial && item.fechaHoyInicio <= paramFechaFinal)
     }
   }
 }
